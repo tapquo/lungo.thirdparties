7991b6d5d527a56ea88742883fd97f847cd688c8
Javier Jiménez Villar <javi.jimenez.villar@gmail.com>
2011-11-04 17:28:15 +0100

    Initial commit version 1.0.0

---
+++ Language/Lungo.Sugar.Language.js
@@ -0,0 +1,52 @@
+/** 
+ * Description or Responsability 
+ * 
+ * @namespace LUNGO.Sugar
+ * @class Language
+ *
+ * @author Javier Jimenez Villar <javi@tapquo.com> || @soyjavi
+ */
+
+LUNGO.Sugar.Language = (function(lng, undefined) {
+    var _languages = {};
+    var _i18n = null;
+
+    /**
+     *
+     *
+     *
+     *
+     */
+    var create = function(i18n, language) {
+        _languages[i18n] = language;
+    };
+
+    /**
+     *
+     *
+     *
+     *
+     */
+    var get = function(i18n) {
+        if (_languages[i18n]) {
+            _i18n = i18n;
+        }
+    };
+
+    /**
+     *
+     *
+     *
+     *
+     */
+    var label = function(key) {
+        return _languages[_i18n][key];
+    };
+
+    return {
+        create: create,
+        get: get,
+        label: label
+    }
+
+})(LUNGO);
