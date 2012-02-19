61c4948f2a4343e62a0ea623d998249f932163dd
Juan Carlos Alonso <me@jcalonso.com>
2012-02-19 19:01:05 +0100

    support for load an array of markers on map, added blue dot for user location marker.
    + support for load an array of markers on map.
    + added blue dot for user location marker.

--- third-parties/Geolocation/lungo.sugar.geolocation.js
+++ third-parties/Geolocation/lungo.sugar.geolocation.js
@@ -12,7 +12,8 @@ LUNGO.Sugar.Geolocation = (function(lng, undefined) {
 	var _container;
 	var _latlng;
 	var _map;
-	var _marker = false;
+	var _userPosition = false;
+	var _markers = false;
 
 	var _onSuccess = function(position) {
 		_latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
@@ -25,12 +26,40 @@ LUNGO.Sugar.Geolocation = (function(lng, undefined) {
 		
 		_map = new google.maps.Map(_container, opt);
 		
-		if(_marker) {
+		if(_userPosition) {
+
+			var image = new google.maps.MarkerImage(
+			  '../assets/images/blue_dot.png',
+			  new google.maps.Size(18,18),
+			  new google.maps.Point(0,0),
+			  new google.maps.Point(9,18)
+			);
+
+
+			var shape = {
+			  coord: [15,1,16,2,16,3,17,4,17,5,17,6,17,7,17,8,17,9,17,10,17,11,17,12,17,13,16,14,16,15,15,16,2,16,1,15,1,14,0,13,0,12,0,11,0,10,0,9,0,8,0,7,0,6,0,5,0,4,1,3,1,2,2,1,15,1],
+			  type: 'poly'
+			};
+
 			var marker = new google.maps.Marker({
 			position: _latlng,
 			map: _map,
-			title: 'Aquí estás tu'
+			title: 'Aquí estás tu',
+			icon:image, 
+			shape:shape
 			});
+		}
+
+		if(_markers != false){
+			for(index in _markers){
+
+				var latLng = new google.maps.LatLng(_markers[index].lat,_markers[index].lng);
+				var marker = new google.maps.Marker({
+				position: latLng,
+				map: _map,
+				title: _markers[index].title
+				});
+			}
 		}
 		
 	};
@@ -43,8 +72,9 @@ LUNGO.Sugar.Geolocation = (function(lng, undefined) {
 		}
 	};
 	
-	var setMap = function(container, marker) {
-		if(marker){_marker = true;}
+	var setMap = function(container, userPosition, markers) {
+		if(userPosition){_userPosition = true;}
+		if(markers != false){_markers = markers;}
 		_container = document.getElementById(container);
 		navigator.geolocation.getCurrentPosition(_onSuccess, _onError);
 	};
